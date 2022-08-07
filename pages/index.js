988519c650a7d7467bbbce76ad6494b696aefc5d
Sanjeevani Bhandari <sanjeevanibhandari3@gmail.com>
2022-08-07 18:51:37 +0530

    frontend folder

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,66 @@ import Image from 'next/image';
 import intro from '../images/join.png';
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import React,{ useState } from 'react';
+
 export default function Home() {
+
+	const[user,setUser]=useState({
+			username: "", 
+			userType: "",
+			email: "",
+			password: "",
+			phone:""			
+		});
+	
+	let name, value;
+	const handleInputs=(e)=>{
+			console.log(e);
+			name=e.target.name;
+			value=e.target.value;
+			
+			setUser({...user,[name]:value})
+	}
+
+	const PostData=async(e)=>{
+		e.preventDefault();
+
+		const {username,userType,email,password,phone}=user;
+
+		const res= await fetch("http://localhost:3000/user/signup",{
+			mode: 'no-cors',
+			method:'POST',
+			headers:{	
+				"Content-Type":"application/json",
+				"Accept": "*/*",
+				"Accept-Encoding":"gzip, deflate, br",
+				"Connection":"keep-alive",
+				"Access-Control-Allow-Origin" : "*", 
+				"Access-Control-Allow-Credentials" : true ,
+			},
+			body :JSON.stringify({
+				
+				username:username,
+				userType:userType,
+				email:email,
+				password:password,
+				phone:phone
+			})
+		});
+		const data =await res.json();
+		if(data.status==500)
+		window.alert("Invalid Registartion");
+		else
+		window.alert("Registartion Successful");
+
+	}
+
+
+
   return (
-    <div class="flex flex-wrap flex-row">
+    <div className="flex flex-wrap flex-row">
   {/* container of image */}
-    <div class='flex w-6/12 h-screen'>
+    <div className='flex w-6/12 h-screen'>
      <Image
       src={intro}
       alt="intro"
@@ -20,32 +75,38 @@ export default function Home() {
 
   {/* starting for signup section  */}
  
-	<div class="justify-center bg-slate-100 rounded-lg mx-20 my-auto w-4/12">
-						<h3 class="pt-10 text-4xl text-center font-bold text-blue-800">Let's Get Started
-            <p class="text-sm text-slate-400 font-normal">Create an account to continue</p></h3>
+	<div className="justify-center bg-slate-100 rounded-lg mx-20 my-auto w-4/12">
+						<h3 className="pt-10 text-4xl text-center font-bold text-blue-800">Let's Get Started
+            <p className="text-sm text-slate-400 font-normal">Create an account to continue</p></h3>
 
-						<form class="px-8 pt-2 pb-8 mb-4 rounded">
-            <div class="grid grid-cols-2 gap-4 my-6">
-								<div class="mr-4">
-									<label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
-										First Name
+						<form className="px-8 pt-2 pb-8 mb-4 rounded" method='POST'>
+            <div className="grid grid-cols-2 gap-4 my-6">
+								<div className="mr-4">
+									<label className="block mb-2 text-sm font-bold text-gray-700" for="username">
+										User Name
 									</label>
 									<input
-										class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="firstName"
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="username"
+										name="username"
 										type="text"
+										value={user.username}
+										onChange={handleInputs}
 										placeholder="Elon"
 									/>
 								</div>
-								<div class="ml-4">
-									<label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
-										Last Name
+								<div className="ml-4">
+									<label className="block mb-2 text-sm font-bold text-gray-700" for="userType">
+										User Type
 									</label>
 									<input
-										class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="lastName"
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="userType"
+										name="userType"
 										type="text"
-										placeholder="Musk"
+										value={user.userType}
+										onChange={handleInputs}
+										placeholder="admin or employee"
 									/>
 								</div>
               </div>
@@ -53,61 +114,70 @@ export default function Home() {
 							
 
               
-							<div class="mb-4">
-								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+							<div className="mb-4">
+								<label className="block mb-2 text-sm font-bold text-gray-700" for="email">
 									Email
 								</label>
 								<input
-									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
 									id="email"
+									name="email"
 									type="email"
+									value={user.email}
+									onChange={handleInputs}
 									placeholder="name@gmail.com"
 								/>
 							</div>
 
 							<div>
-								<div class="mb-4">
-									<label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+								<div className="mb-4">
+									<label className="block mb-2 text-sm font-bold text-gray-700" for="password">
 										Password
 									</label>
 									<input
-										class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
 										id="password"
+										name="password"
 										type="password"
+										value={user.password}
+										onChange={handleInputs}
 										placeholder="at least 8 characters"
 									/>
-									{/* <p class="text-xs italic text-red-500">Please choose a password.</p> */}
+									{/* <p className="text-xs italic text-red-500">Please choose a password.</p> */}
 								</div>
 								<div>
-									<label class="block mb-2 text-sm font-bold text-gray-700" for="c_password">
-										Confirm Password
+									<label className="block mb-2 text-sm font-bold text-gray-700" for="phone">
+										Phone Number
 									</label>
 									<input
-										class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="c_password"
-										type="password"
-										placeholder="at least 8 characters"
+										className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="phone"
+										name="phone"
+										type="Number"
+										value={user.phone}
+										onChange={handleInputs}
+										placeholder="Your Phone Number"
 									/>
 								</div>
 							</div>
-              <div class="mb-8"><input type="checkbox" class="checked:bg-blue-500" />
-              <label for="checkbox" class="ml-2 text-xs text-gray-500">By creating an account you agree with our <span class="font-bold text-blue-600">Terms and Conditions</span>.</label>
+              <div className="mb-8"><input type="checkbox" className="checked:bg-blue-500" />
+              <label for="checkbox" className="ml-2 text-xs text-gray-500">By creating an account you agree with our <span className="font-bold text-blue-600">Terms and Conditions</span>.</label>
               </div>
               
-							<div class="mb-6 text-center">
+							<div className="mb-6 text-center">
 								<button
-									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-									type="button"
+									className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+									type="button" onClick={PostData}
 								>
 									Create Account
 								</button>
 							</div>
-							<hr class="mb-2 border-t" />
+							<hr className="mb-2 border-t" />
 							
-							<div class="text-center">
+							<div className="text-center">
               <Link href="/login">
                             <a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
 									
 								>
 									Already have an account? Login!
--- pages/login.js
+++ pages/login.js
@@ -4,11 +4,12 @@ import intro from '../images/welcome.png';
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 
+
 export default function Home() {
   return (
-    <div class="flex flex-wrap flex-row">
+    <div className="flex flex-wrap flex-row">
   {/* container of image */}
-    <div class='flex w-6/12 h-screen'>
+    <div className='flex w-6/12 h-screen'>
      <Image
       src={intro}
       alt="intro"
@@ -21,63 +22,63 @@ export default function Home() {
 
   {/* starting for signup section  */}
  
-	<div class=" bg-slate-100 rounded-lg mx-20 my-auto w-4/12">
-			<h3 class="pt-10 text-4xl text-center font-bold text-blue-800 mb-4">Hello Again!!
-            <p class="text-sm text-slate-400 font-normal">Login into you account</p></h3>
+	<div className=" bg-slate-100 rounded-lg mx-20 my-auto w-4/12">
+			<h3 className="pt-10 text-4xl text-center font-bold text-blue-800 mb-4">Hello Again!!
+            <p className="text-sm text-slate-400 font-normal">Login into you account</p></h3>
 
-						<form class="px-8 pt-2 pb-8 mb-4 rounded">
+						<form className="px-8 pt-2 pb-8 mb-4 rounded">
            
 
 							
 
               
-							<div class="mb-4">
-								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
-									Email
+							<div className="mb-4">
+								<label className="block mb-2 text-sm font-bold text-gray-700" for="username">
+									User Name
 								</label>
 								<input
-									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="email"
-									type="email"
+									className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="username"
+									type="text"
 									placeholder="name@gmail.com"
 								/>
 							</div>
 
-							<div class="mb-8">
-								<div class="mb-4">
-									<label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+							<div className="mb-8">
+								<div className="mb-4">
+									<label className="block mb-2 text-sm font-bold text-gray-700" for="password">
 										Password
 									</label>
 									<input
-										class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
 										id="password"
 										type="password"
 										placeholder="at least 8 characters"
 									/>
-									{/* <p class="text-xs italic text-red-500">Please choose a password.</p> */}
+									{/* <p className="text-xs italic text-red-500">Please choose a password.</p> */}
 								</div>	
 							</div>
-							<div class="mb-6 text-center">
+							<div className="mb-6 text-center">
 								<button
-									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+									className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
 									type="button"
 								>
 									Log In
 								</button>
 							</div>
-							<hr class="mb-6 border-t" />
-							<div class="text-center">
+							<hr className="mb-6 border-t" />
+							<div className="text-center">
 								<a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
 									href="#"
 								>
 									Forgot Password?
 								</a>
 							</div>
-							<div class="text-center">
+							<div className="text-center">
                            
                                 <Link href="/">
-                <a class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
+                <a className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
 									Don't have an account? Sign Up!
 								</a>
               </Link>
